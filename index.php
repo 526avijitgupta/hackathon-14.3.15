979bb97a8a7c8ede5c15b36ee2e528af8c8454a1
Avijit Gupta <526avijitgupta@gmail.com>
2015-03-14 15:19:29 +0530

    Updated to accomodate Codecha

--- index.php
+++ index.php
@@ -2,11 +2,6 @@
   <head>
     <meta charset="utf-8">
     <title>Hackathon - 14/3/15</title>
-
-
- 	<script type="text/javascript" src="//codecha.org/api/challenge?k=cc1eaed32e504532975bb0c54982da41"> </script>
-
-
   </head>
   <body>
     <div>
@@ -14,6 +9,7 @@
 	<input name="name" type="text" placeholder="Name">
 	<input name="email" type="email" placeholder="Email">
 	<input name="password" type="password" placeholder="Password">
+	<script type="text/javascript" src="http://codecha.org/api/challenge?k=cc1eaed32e504532975bb0c54982da41"></script>	
 	<button type="submit">Submit</button>
       </form>
     </div>
@@ -28,12 +24,12 @@
 	        $anws = codecha_check($_POST["codecha_challenge_field"],
 	                $_POST["codecha_response_field"],
 	                $_SERVER["REMOTE_ADDR"],
-	                "YOUR_PRIVATE_KEY");
+	                "55f57a95750b4d67b3adf81ab9d6d1c5");
 	} elseif (isset($_POST["recaptcha_challenge_field"]) && isset($_POST["recaptcha_response_field"]) ) {
 	        $anws = recaptcha_check($_POST["recaptcha_challenge_field"],
 	                $_POST["recaptcha_response_field"],
 	                $_SERVER["REMOTE_ADDR"],
-	                "YOUR_PRIVATE_KEY");
+	                "55f57a95750b4d67b3adf81ab9d6d1c5");
 	}
 
 	if ($anws) {
