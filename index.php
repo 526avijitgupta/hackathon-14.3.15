fa0af4e25b36b0e6e80b7de0ebbc31653402d5ab
Avijit Gupta <526avijitgupta@gmail.com>
2015-03-14 14:57:55 +0530

    Added basic form

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Hackathon - 14/3/15</title>
+  </head>
+  <body>
+    <div>
+      <form action="submit.php">
+	<input name="name" type="text" placeholder="Name">
+	<input name="email" type="email" placeholder="Email">
+	<input name="password" type="password" placeholder="Password">
+	<button type="submit">Submit</button>
+      </form>
+    </div>
+  </body>
+</html>
